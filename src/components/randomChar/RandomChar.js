dc85986beab6663806d64c174150d3e26686e2fc
Max <max11cz@seznam.cz>
2022-10-09 15:49:43 +0200

    test

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -13,20 +13,21 @@ const RandomChar = () => {
     useEffect(() => {
         updateChar();
         const timerId = setInterval(updateChar, 60000);
-
         return () => {
             clearInterval(timerId)
         }
     }, []);
   
+
+    const onCharLoaded = (char) =>{
+        setChar(char);
+    }
+
     const updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000)+1011000);
         getCharacter(id).then(onCharLoaded);
     }
 
-    const onCharLoaded = (char) =>{
-        setChar(char);
-    }
 
     const errorMessage = error ? <ErrorMessage/> : null;
     const spinner = loading ? <Spinner/> : null;
